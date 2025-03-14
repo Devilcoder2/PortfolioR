a834b46f5ab739aed61503fadcff305e8bf72c67
raman-mapsense <210108077@HBTU.AC.IN>
2025-03-14 17:04:42 +0530

    env

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,30 +5,24 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://19d38b172429d716faa70c88a8860c61@o4507394710962176.ingest.de.sentry.io/4507394714697808",
+  dsn: "https://86d725697eb4f7946d1dc7fbac99ba66@o4508975872933888.ingest.de.sentry.io/4508975875162192",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Add optional integrations for additional features
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-
-  replaysOnErrorSampleRate: 1.0,
-
+  // Define how likely Replay events are sampled.
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
 
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    Sentry.replayIntegration({
-      // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
-    }),
-    Sentry.feedbackIntegration({
-      // Additional SDK configuration goes in here, for example:
-      colorScheme: "dark",
-    }),
-  ],
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,15 +5,11 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://19d38b172429d716faa70c88a8860c61@o4507394710962176.ingest.de.sentry.io/4507394714697808",
+  dsn: "https://86d725697eb4f7946d1dc7fbac99ba66@o4508975872933888.ingest.de.sentry.io/4508975875162192",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
 });
