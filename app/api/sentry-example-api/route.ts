3c061b2c50f9e1374e3e665d25b54586e6f5cbb7
raman-mapsense <210108077@HBTU.AC.IN>
2025-03-14 17:09:29 +0530

    Update route.ts

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -2,8 +2,11 @@ import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-// A faulty API route to test Sentry's error monitoring
 export function GET() {
-  throw new Error("Sentry Example API Route Error");
-  return NextResponse.json({ data: "Testing Sentry Error..." });
+  try {
+    throw new Error("Sentry Example API Route Error"); // Simulating an error
+  } catch (error) {
+    console.error("API Route Error:", error);
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
 }
